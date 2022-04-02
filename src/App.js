03be0bafbadd84e14b7da567698e1abcfad93349
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-04-02 14:56:06 +0100

    router on

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
+import { BrowserRouter as  Router, Routes,Route } from 'react-router-dom';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-<h1>hI</h1>
+      <Router>
+      <Header />
+      <Routes>
+      <Route path="/" exact component={ProductListing} />
+      <Route path="/product/:productId" exact component={ProductDetail} />
+      <Route> 404 NOT FOUND!</Route>
+      </Routes>
+      </Router>
+
     </div>
   );
 }
---
+++ src/containers/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+return (
+    <div className="uifixed menu">
+        <div className="ui container center">
+            <h2>Shoopers</h2>
+        </div>
+    </div>
+)
+}
+
+export default Header
