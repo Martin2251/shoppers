38cef0de03194fb314fbc0d705ab546f06271742
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-04-02 14:10:46 +0100

    COMMIT

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,15 @@
+// 3 ACTIONS SET SELECTED AND REMOVE 
+import { ActionTypes } from "../contants/action-types"
+export const setProducts = (products) => {
+    return {
+        type: ActionTypes.SET_PRODUCTS,
+        payload: products,
+    };
+};
+
+export const selectedProduct = (product) => {
+    return {
+        type: ActionTypes.SELECTED_PRODUCTS,
+        payload: product,
+    };
+};
